b3c9a8301a1988e1431f3f3c2652aee49b1567dc
dvorakjs <dvorakjs@gmail.com>
2023-05-30 14:28:56 +0900

    Updates

---
+++ precache-manifest.ad3033912fcf835586212defcff61055.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "911073dae444b04c04ea",
+    "url": "/mysite/static/css/main.7946cbe8.chunk.css"
+  },
+  {
+    "revision": "911073dae444b04c04ea",
+    "url": "/mysite/static/js/main.911073da.chunk.js"
+  },
+  {
+    "revision": "3e3c3eb55c09e01a1300",
+    "url": "/mysite/static/js/1.3e3c3eb5.chunk.js"
+  },
+  {
+    "revision": "ff813772de7efb309245",
+    "url": "/mysite/static/js/2.ff813772.chunk.js"
+  },
+  {
+    "revision": "56024b5d937d1eee2aed",
+    "url": "/mysite/static/js/runtime~main.56024b5d.js"
+  },
+  {
+    "revision": "1e15d127e8967ca14d63ddf8fd385984",
+    "url": "/mysite/index.html"
+  }
+];
